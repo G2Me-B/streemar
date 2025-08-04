ec0954bf8a1c891036c808fb793d889617bd6994
G2Me-B <brutehackenstein@gmail.com>
2025-08-04 19:29:40 +0000

    pages setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Routes, Route } from "react-router"
+import toast, {Toaster} from "react-hot-toast"
+
+import HompePage from "./pages/HomePage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import NotificationsPage from "./pages/NotificationsPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className=" h-screen" data-themes="night">
+      <button onClick={()=> toast.success("Hello World")}>Create a toast</button>
+
+      <Routes>
+        <Route path="/" element={<HompePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
+
+
+      <Toaster />
+    </div>
   )
 }
 
