125a99d2c9a934d6ea89c16f4a48f5ba0f3981cb
G2Me-B <brutehackenstein@gmail.com>
2025-08-17 22:38:14 +0000

    updated

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("streamer-theme") || "aqua",
+    theme: localStorage.getItem("streamer-theme") || "black",
     setTheme: (theme) => {
         localStorage.setItem("streamer-theme", theme);
         set({ theme });
