6ee8b968bb292f48c0f2b55d7c42d5d05b0558d6
G2Me-B <brutehackenstein@gmail.com>
2025-08-18 21:07:11 +0000

    changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,7 +78,7 @@ export async function login(req, res) {
         }
 
         const user = await User.findOne({ email })
-        if (!user) return res.status(401).json({ message: "Invalid email or passwprd " })
+        if (!user) return res.status(401).json({ message: "Invalid email or password " })
 
         const isPasswordCorrect = await user.matchPassword(password)
         if (!isPasswordCorrect) return res.status(401).json({ message: "Invalid email or password" })
